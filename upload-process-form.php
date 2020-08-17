c1d82e183a1224cc58a7d3bb60e9dfef5fe7f6cf
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2020-08-17 13:15:56 -0400

    Update upload-process-form.php

--- upload-process-form.php
+++ upload-process-form.php
@@ -1230,8 +1230,8 @@ while( $row = $statement->fetch() ) {
 
 
 					<div class="list_mass_members"> <!-- <a href="#" class="btn btn-xs btn-primary add-all" data-type="assigns">
-						<?php //_e('Add all','cftp_admin'); ?> -->
-						</a> <a href="#" class="btn btn-xs btn-primary remove-all" data-type="assigns">
+						<?php //_e('Add all','cftp_admin'); ?> </a>  -->
+						<a href="#" class="btn btn-xs btn-primary remove-all" data-type="assigns">
 						<?php _e('Remove all','cftp_admin'); ?>
 						</a> <a href="#" class="btn btn-xs btn-danger copy-all" data-type="assigns">
 						<?php _e('Copy selections to other files','cftp_admin'); ?>
@@ -1288,8 +1288,8 @@ while( $row = $statement->fetch() ) {
 
 
 			<div class="list_mass_members"> <!-- <a href="#" class="btn btn-xs btn-primary add-all" data-type="categories">
-				<?php //_e('Add all','cftp_admin'); ?> -->
-				</a> <a href="#" class="btn btn-xs btn-primary remove-all" data-type="categories">
+				<?php //_e('Add all','cftp_admin'); ?> </a> -->
+				<a href="#" class="btn btn-xs btn-primary remove-all" data-type="categories">
 				<?php _e('Remove all','cftp_admin'); ?>
 				</a> <a href="#" class="btn btn-xs btn-danger copy-all" data-type="categories">
 				<?php _e('Copy selections to other files','cftp_admin'); ?>
