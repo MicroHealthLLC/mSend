e80cc1dfa3d0dcad46eb08a65ac396561efea113
bluenevus <frank.tucker@microhealthllc.com>
2020-10-31 14:56:39 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,8 +14,8 @@ echo "$(timestamp): Deploying mSend"
 
 cd /var/www/mSend \
 && sudo git pull \
-&& chown -R chown -R apache:apache *
-&& apachectl restart
+&& chown -R chown -R apache:apache * \
+&& apachectl restart \
 
 echo "$(timestamp): mSend Deployed"
 
